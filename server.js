b37dfdbf1c452149d9a066d7aacb605a8c92cb69
parthamv <mvgopesh@gmail.com>
2025-06-04 22:56:54 -0400

    Changes in the port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import routes from './routes.js';
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 80;
 
 // Use routes
 app.use('/api', routes);
